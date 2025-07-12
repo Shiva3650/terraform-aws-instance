0dbd8a5bb0b85f65f83f3882863b084e44e12464
Shiva <nzshivap@gmail.com>
2025-07-12 23:34:08 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "t3_small" {
+  ami           = var.ami_id
+  instance_type = "t3.small"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "TerraformT3Instance"
+  }
+}
 

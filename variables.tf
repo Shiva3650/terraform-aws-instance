e6b01f12aff594029ff6149a00371c3eb24ccb9e
Shiva <nzshivap@gmail.com>
2025-07-16 23:50:10 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  default     = "ap-south-1"
-}
-
-variable "ami_id" {
-  description = "AMI ID to use for the instance"
-  type        = string
-}
-
-variable "key_name" {
-  description = "SSH key name"
-  type        = string
+variable "ec2_name" {
+  type = string
 }

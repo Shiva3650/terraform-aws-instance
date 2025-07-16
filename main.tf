dc996e457b02a81366fe73b09a0dc482754466c9
Shiva <nzshivap@gmail.com>
2025-07-16 23:49:42 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
-provider "aws" {
-  region = var.aws_region
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+
+    filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["aroura"]
+    }
+    
+    owners = ["AWSAroura"] # Canonical
 }
 
-resource "aws_instance" "t3_small" {
-  ami           = var.ami_id
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "Arora_server" {
+  ami           = data.ami-0a1235697f4afa8a4
   instance_type = "t3.small"
-  key_name      = var.key_name
+  key_name      = "ec2-ssh-key"
 
   tags = {
-    Name = "TerraformT3Instance"
+    Name = var.ec2_name
   }
 }
-
